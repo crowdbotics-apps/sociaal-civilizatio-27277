3ca4ffb289f81be9cc88804aabb458f79a141a33
Crowdbotics <team@crowdbotics.com>
2021-05-23 14:46:23 +0000

    Initial commit.

---
+++ backend/sociaal_civilizatio_27277/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sociaal_civilizatio_27277 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sociaal_civilizatio_27277.settings')
+
+application = get_wsgi_application()
